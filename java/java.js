5c5394327e768f93da14bc5da757a0eb84af3532
Francesca Palumbo <econofra87@gmail.com>
2020-04-23 00:33:13 +0200

    non ho capito nulla

---
+++ java/java.js
@@ -0,0 +1,29 @@
+// Scrivi un programma che stampi i numeri da 1 a 100, con le seguenti regole:
+// al posto dei multipli di 3 stampi "Fizz"
+// al posto dei multipli di 5 stampi "Buzz"
+// al posto dei multipli sia di 3 che di 5 stampi "FizzBuzz"
+
+//inizio l'esercizio impostando l'elenco dei numeri da 1 a 100
+// for (var i = 1; i < 100; i++) {
+//     console.log(i);
+// }
+//
+// //  creo l'array per procedere con le iterazioni
+//  var lista_numeri = [ i ];
+//     console.log(i);
+//
+// for (var i = 0; i < lista_numeri.length; i++) {
+//     console.log( lista_numeri [i]);
+// }
+//
+// // definisco la varibile per i multipli di 3
+// var multiplo_di_tre = i+2;
+//     console.log( "Fizz" );
+// if (i+2) {
+//     console.log( "Fizz");
+// }
+
+// definisco la varibile per i multipli di 5
+// definisco la varibile per i multipli di 15
+
+var lista_numeri 
